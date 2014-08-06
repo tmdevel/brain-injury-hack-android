121ac00eaa2051a87a97b3d47ec4b8d9e89318d6
sarah.dynan <sarah.dynan@dev-mac-02.task.local>
2014-08-06 17:06:53 +0100

    Image view fragment

--- BrainInjuryApp/src/com/totalmobile/risksapp/LeafletListFragment.java
+++ BrainInjuryApp/src/com/totalmobile/risksapp/LeafletListFragment.java
@@ -3,7 +3,6 @@ package com.totalmobile.risksapp;
 import java.util.ArrayList;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
--- BrainInjuryApp/src/com/totalmobile/risksapp/ViewPagerAdapter.java
+++ BrainInjuryApp/src/com/totalmobile/risksapp/ViewPagerAdapter.java
@@ -1,6 +1,5 @@
 package com.totalmobile.risksapp;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -10,21 +9,11 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 	private int[] imageResources;
 	private int ADAPTER_SIZE = 0;
 
-	EmptyFragment[] fragments = new EmptyFragment[8];
-
 	public ViewPagerAdapter(FragmentManager fm, int[] imageResources) {
 		super(fm);
 
 		this.imageResources = imageResources;
 		ADAPTER_SIZE = imageResources.length + 1;
-
-		for (int i = 0; i < ADAPTER_SIZE; i++) {
-			Bundle args = new Bundle();
-			args.putString("textView", i + "");
-			EmptyFragment fragment = new EmptyFragment();
-			fragment.setArguments(args);
-			fragments[i] = fragment;
-		}
 	}
 
 	@Override
@@ -32,8 +21,7 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 		if (position == ADAPTER_SIZE - 1) {
 			return new LeafletListFragment();
 		} else {
-			// return image fragment
-			return fragments[position];
+			return ImageFragment.newInstance(imageResources[position]);
 		}
 	}
 
